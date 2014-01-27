64571851028ed2e8d1caaa3c364537d4023109eb
Jason Walsh <j@wal.sh>
2014-01-27 16:13:54 -0500

    Show the account information with the additional parameters
    https://www.wepay.com/developer/reference/account

--- bin/api-user.sh
+++ bin/api-user.sh
@@ -1,6 +1,34 @@
 #!/bin/sh
+# Simplify the reporting on the user accounts we care about on
+# staging:
 
-curl https://stage.wepayapi.com/v2/user/ \
-    -H "Authorization: Bearer $TOKEN"
+# These are just pulled from the API calls that only expected the
+# access token (.com/developer/reference/access-tokens)
 
-# {"user_id":168222854,"first_name":"Jason","last_name":"Walsh","user_name":"Jason Walsh","email":"jwalsh@booster.com","state":"registered"}%
+# user: https://www.wepay.com/developer/reference/user (modification,
+# registration, confirmation)
+
+for E in user account/find
+do
+    echo
+    echo $E
+    curl https://stage.wepayapi.com/v2/$E \
+        -H "Authorization: Bearer $TOKEN"
+
+done
+
+# Output: Sample output for the user should look something like
+# {"user_id":168222854,"first_name":"Jason",
+#  "last_name":"Walsh","user_name":"Jason Walsh",
+#  "email":"jwalsh@booster.com","state":"registered"}
+
+# These calls require additional parameters in addition to the
+# authorization header
+
+for E in account
+do
+    echo
+    echo $E
+    curl --data "account_id=$ACCOUNT" https://stage.wepayapi.com/v2/$E \
+        -H "Authorization: Bearer $TOKEN"
+done
